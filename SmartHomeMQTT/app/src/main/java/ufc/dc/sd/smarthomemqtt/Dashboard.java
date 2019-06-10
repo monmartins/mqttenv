2f5559861941ec27eae301cb95cbfcb9ce9a716a
Katharine Amaral <katharines2@hotmail.com>
2019-06-09 22:45:20 -0300

    alterações
    conferir imports

--- SmartHomeMQTT/app/src/main/java/ufc/dc/sd/smarthomemqtt/Dashboard.java
+++ SmartHomeMQTT/app/src/main/java/ufc/dc/sd/smarthomemqtt/Dashboard.java
@@ -1,5 +1,6 @@
-package ufc.dc.sd.smarthomesocket;
+package ufc.dc.sd.smarthomemqtt;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,6 +15,8 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import ufc.dc.sd.smarthomesocket.R;
+
 public class Dashboard extends AppCompatActivity {
 
     // initiate a Switch
@@ -22,6 +25,18 @@ public class Dashboard extends AppCompatActivity {
     Switch switchArCondicionado;
     Switch switchTV;
     Switch switchSom;
+
+    String topicoPorta = "door";
+    String topicoLampada = "lamp";
+    String topicoArCondicionado = "ar";
+    String topicoTV = "tv";
+    String topicoSom = "sound";
+
+    MqttAndroidClient mqttAndroidClient;
+
+    final String serverUri = "tcp://iot.eclipse.org:1883";
+
+    String clientId = "Cliente";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +51,75 @@ public class Dashboard extends AppCompatActivity {
         switchTV = (Switch) findViewById(R.id.switchTV);
         switchSom = (Switch) findViewById(R.id.switchSom);
 
+
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+                if (reconnect) {
+                    addToHistory("Reconnected to : " + serverURI);
+                    subscribeToTopic();
+                } else {
+                    addToHistory("Connected to: " + serverURI);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                addToHistory("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                addToHistory("Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
+        try {
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to connect to: " + serverUri);
+                }
+            });
+
+        } catch (MqttException ex){
+            ex.printStackTrace();
+        }
+
+
         switchPorta.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                subscribeToTopic(topicoPorta);
                 if(isChecked) {
                     Toast.makeText(getApplicationContext(),"Porta aberta!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoPorta, 1);
                     //Add chamada metodo abrir porta
                 }
                 else {
                     Toast.makeText(getApplicationContext(),"Porta fechada!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoPorta, 0);
                     //Add chamada metodo fechar porta
                 }
             }
@@ -53,12 +128,15 @@ public class Dashboard extends AppCompatActivity {
         switchLampadas.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                subscribeToTopic(topicoLampada);
                 if(isChecked) {
                     Toast.makeText(getApplicationContext(),"Lâmpadas ligadas!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoLampada, 1);
                     //Add chamada metodo ligar lâmpadas
                 }
                 else {
                     Toast.makeText(getApplicationContext(),"Lâmpadas desligadas!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoLampada, 0);
                     //Add chamada metodo desligar lâmpadas
                 }
             }
@@ -67,12 +145,15 @@ public class Dashboard extends AppCompatActivity {
         switchArCondicionado.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                subscribeToTopic(topicoArCondicionado);
                 if(isChecked) {
                     Toast.makeText(getApplicationContext(),"Ar condicionado ligado!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoArCondicionado, 1);
                     //Add chamada metodo ligar ar condicionado
                 }
                 else {
                     Toast.makeText(getApplicationContext(),"Ar condicionado desligado!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoArCondicionado, 0);
                     //Add chamada metodo desligar ar condicionado
                 }
             }
@@ -81,12 +162,15 @@ public class Dashboard extends AppCompatActivity {
         switchTV.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                subscribeToTopic(topicoTV);
                 if(isChecked) {
                     Toast.makeText(getApplicationContext(),"TV ligada!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoTV, 1);
                     //Add chamada metodo ligar TV
                 }
                 else {
                     Toast.makeText(getApplicationContext(),"TV desligada!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoTV, 0);
                     //Add chamada metodo desligar TV
                 }
             }
@@ -95,15 +179,53 @@ public class Dashboard extends AppCompatActivity {
         switchSom.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                subscribeToTopic(topicoSom);
                 if(isChecked) {
                     Toast.makeText(getApplicationContext(),"Som ligado!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoSom, 1);
                     //Add chamada metodo ligar som
                 }
                 else {
                     Toast.makeText(getApplicationContext(),"Som desligado!", Toast.LENGTH_LONG).show();
+                    publishMessage(topicoSom, 0);
                     //Add chamada metodo desligar som
                 }
             }
         });
+    }
+
+    public void subscribeToTopic(String subscriptionTopic){
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    System.out.println("Inscrito no tópico");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    System.out.println("Falha na inscrição");
+                }
+            });
+
+        } catch (MqttException ex){
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    public void publishMessage(String topic, Integer valor){
+        try {
+            MqttMessage message = new MqttMessage();
+            message.setPayload(publishMessage.getBytes());
+            mqttAndroidClient.publish(topic, valor, message);
+            System.out.println("Mensagem Publicada");
+            if(!mqttAndroidClient.isConnected()){
+                System.out.println(mqttAndroidClient.getBufferedMessageCount() + " mensagens no buffer.");
+            }
+        } catch (MqttException e) {
+            System.err.println("Erro Publicando: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
